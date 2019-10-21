871bf825a108c4def22480afbadcd7b0cff7dbe6
innerWang <52886377@qq.com>
2019-10-21 11:00:57 +0800

    feat: add pre-commit lint

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-// import { TransitionMotion } from "react-motion";
+import { TransitionMotion } from 'react-motion'
 
 const TransitionMotionExample = () => {
-  // const [pos, setPos] = useState({ x: 0, y: 0, now: "t0" });
+  // const [pos, setPos] = useState({ x: 0, y: 0, now: 't0' })
 
-  // const willLeave = styleThatLeft => {
-  //   return {};
-  // };
+  const willLeave = () => {
+    return {}
+  }
 
-  // const getStyles = () => {
-  //   return;
-  // };
+  const getStyles = () => {}
 
   const handleMouseMove = e => {
     console.log(e) // eslint-disable-line
@@ -20,9 +18,9 @@ const TransitionMotionExample = () => {
     <div className="main-board">
       <h2> 我是 TransitionMotion 应用示例</h2>
       <section className="trans-sec" onMouseMove={handleMouseMove}>
-        {/* <TransitionMotion styles={getStyles} willLeave={willLeave}>
+        <TransitionMotion styles={getStyles} willLeave={willLeave}>
           {}
-        </TransitionMotion> */}
+        </TransitionMotion>
       </section>
     </div>
   )
